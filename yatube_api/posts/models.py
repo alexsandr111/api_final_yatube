15daf8d7eb50325205504cf9ca698ecbcd8af9d1
Khodeev Alexander <akhodeev.junior@gmail.com>
2025-01-26 17:34:48 +0300

    третий коммит

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 User = get_user_model()
 
+
 class Group(models.Model):
     """Модель групп"""
 
@@ -13,12 +14,14 @@ class Group(models.Model):
     def __str__(self):
         return self.title
 
+
 class Post(models.Model):
     """Модель постов"""
 
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User,
+        on_delete=models.CASCADE, related_name='posts')
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
@@ -36,16 +39,18 @@ class Post(models.Model):
 class Comment(models.Model):
     """Модель комментариев"""
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User,
+        on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post,
+        on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
 
     def __str__(self):
         return self.text
-    
-    
+
+
 class Follow(models.Model):
     """Модель подписок"""
 

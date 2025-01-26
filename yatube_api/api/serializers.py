4eb7bbf56afcf111331654c1dc9d632f4fea9471
Khodeev Alexander <akhodeev.junior@gmail.com>
2025-01-26 17:50:33 +0300

    4й коммит

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -7,9 +7,13 @@ from posts.models import Comment, Follow, Group, Post, User
 class PostSerializer(serializers.ModelSerializer):
     """Сериализатор постов"""
 
-    author = serializers.SlugRelatedField(slug_field='username',read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
+
         model = Post
         fields = ['id', 'text', 'pub_date', 'author', 'group', 'image']
 
@@ -17,18 +21,26 @@ class PostSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     """Сериализатор комментариев"""
 
-    author = serializers.SlugRelatedField(read_only=True,slug_field='username')
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
 
     class Meta:
+
         model = Comment
         fields = ['id', 'author', 'post', 'text', 'created']
         read_only_fields = ['author', 'post']
 
+
 class GroupSerializer(serializers.ModelSerializer):
-    """Сериализатор групп"""
+    """Сериализатор групп."""
+
     class Meta:
+
         model = Group
         fields = ['id', 'title', 'slug', 'description']
+
 
 class FollowSerializer(serializers.ModelSerializer):
     """Сериализатор подписок"""
@@ -45,19 +57,24 @@ class FollowSerializer(serializers.ModelSerializer):
 
     class Meta:
 
-        fields = ('user','following')
+        fields = (
+            'user',
+            'following'
+        )
         model = Follow
 
     validators = [
         UniqueTogetherValidator(
             queryset=Follow.objects.all(),
-            fields=('user','following'),
+            fields=(
+                'user',
+                'following'
+            ),
             message='Подписка уже совершена'
         )
     ]
 
     def validate(self, data):
-        """Валидация"""
         if self.context['request'].user == data['following']:
             raise serializers.ValidationError(
                 'Подписка на самого себя недоступна.'
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -20,8 +20,8 @@ class Post(models.Model):
 
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(User,
-        on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
@@ -39,10 +39,10 @@ class Post(models.Model):
 class Comment(models.Model):
     """Модель комментариев"""
 
-    author = models.ForeignKey(User,
-        on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(Post,
-        on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
